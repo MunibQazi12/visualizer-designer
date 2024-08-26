48c1e9507fb26ce62218f764b7ce4668ea3afeb7
MunibQazi12 <munibqazi770@gmail.com>
2024-08-26 13:11:33 +0500

    components division

---
+++ public/svgs/scroll-down.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ScrollDownSvg = () => {
+  return (
+    <svg
+      className="absolute bottom-0 left-0 right-0 mx-auto z-10"
+      width="39"
+      height="17"
+      viewBox="0 0 39 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M36 3L19.3493 14M3 3L19.0541 13.7481"
+        stroke="white"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default ScrollDownSvg;
---
+++ src/components/step-details/kitchen/index.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import MapSvg from "@/../public/svgs/map";
+import EyeSvg from "@/../public/svgs/eyes";
+import AccordionOpenSvg from "@/../public/svgs/accordion-open";
+
+interface I_Props {
+  setSelectedKitchenType: any;
+  selectedKitchenType: any;
+}
+
+const Kitchen = (props: I_Props) => {
+  const { setSelectedKitchenType, selectedKitchenType } = props;
+
+  return (
+    <>
+      <div className="flex items-center">
+        <div className="w-5 h-5 rounded-full bg-green-500 p-[5px]">
+          <span className="block w-full h-full rounded-full bg-white"></span>
+        </div>
+        <p className="w-[calc(100%-113px)] pl-4 pr-2 text-lg font-medium leading-[30px] text-primary-100 mb-0">
+          Kitchen
+        </p>
+        <div className="flex items-center gap-[11px]">
+          <button type="button">
+            <MapSvg />
+          </button>
+          <button type="button">
+            <EyeSvg />
+          </button>
+        </div>
+        <button type="button" className="ml-6">
+          <AccordionOpenSvg />
+        </button>
+      </div>
+
+      <div className="mt-4 pl-5">
+        <div className="inline-flex items-center">
+          <div
+            className={`w-4 h-4 rounded-full p-1 ${
+              selectedKitchenType.kitchen_layout
+                ? "bg-green-500"
+                : "bg-gray-400"
+            }`}
+          >
+            <span className="block w-full h-full rounded-full bg-white"></span>
+          </div>
+          <p
+            className="text-base font-normal leading-6 text-primary-100 mb-0 pl-[13px] cursor-pointer"
+            onClick={() => setSelectedKitchenType("kitchen_layout")}
+          >
+            Kitchen Layout
+          </p>
+        </div>
+        <div className="flex items-center">
+          <div
+            className={`w-4 h-4 rounded-full p-1 ${
+              selectedKitchenType.cabinet_selection
+                ? "bg-green-500"
+                : "bg-gray-400"
+            }`}
+          >
+            <span className="block w-full h-full rounded-full bg-white"></span>
+          </div>
+          <p
+            className="text-base font-normal leading-6 text-primary-100 mb-0 pl-[13px] cursor-pointer"
+            onClick={() => setSelectedKitchenType("cabinet_selection")}
+          >
+            Cabinet Selection
+          </p>
+        </div>
+        <div className="flex items-center">
+          <div
+            className={`w-4 h-4 rounded-full p-1 ${
+              selectedKitchenType.counterTop_selection
+                ? "bg-green-500"
+                : "bg-gray-400"
+            }`}
+          >
+            <span className="block w-full h-full rounded-full bg-white"></span>
+          </div>
+          <p
+            className="text-base font-normal leading-6 text-primary-100 mb-0 pl-[13px] cursor-pointer"
+            onClick={() => setSelectedKitchenType("counterTop_selection")}
+          >
+            Countertop Selection
+          </p>
+        </div>
+        <div className="flex items-center">
+          <div
+            className={`w-4 h-4 rounded-full  p-1 ${
+              selectedKitchenType.plumbing_fixture
+                ? "bg-green-500"
+                : "bg-gray-400"
+            }`}
+          >
+            <span className="block w-full h-full rounded-full bg-white"></span>
+          </div>
+          <p
+            className="text-base font-normal leading-6 text-primary-100 mb-0 pl-[13px] cursor-pointer"
+            onClick={() => setSelectedKitchenType("plumbing_fixture")}
+          >
+            Plumbing Fixtures
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Kitchen;
