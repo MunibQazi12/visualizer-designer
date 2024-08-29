8372dc4d0f9b7eb97c01f9e2306580445f228618
MunibQazi12 <munibqazi770@gmail.com>
2024-08-29 16:40:16 +0500

    settings card scroll issue fixed

--- src/components/meterial-preview/setting-card/index.tsx
+++ src/components/meterial-preview/setting-card/index.tsx
@@ -17,28 +17,30 @@ const SettingCard = (props: I_Props) => {
     return (
       <div style={{width: "500px"}}>
         <div className={`flex justify-end pr-[33px] transition`}>
-          <div className="bg-customWhite-600 mr-4 2xl:w-[414px] h-[493px] overflow-auto py-[35px] px-[38px] rounded-[28px] scrollBar-hidden">
+          <div className="bg-customWhite-600 mr-4 w-[414px] h-[493px] overflow-auto py-[35px] px-[38px] rounded-[28px] scrollBar-hidden">
             <p className="text-primary-100 font-semibold text-lg leading-[27px] font-roboto mb-[7px]">
               {!!selectedVarientElements.length && selectedVarientElements[0][" Opt Sel Name"]}
             </p>
-            <div className=""> 
+            <div> 
               <p className="text-primary-100 font-semibold text-sm leading-[21px] font-roboto mb-0">
                 Active Selection
               </p>
-              <div className="flex flex-wrap gap-[22px] mt-[15px]">
-                {selectedVarientElements.map((variant: any) => {
-                  return <>
-                    <Selections
-                      products={variant}
-                    />
-                    {/* <div className="py-4">
+              <div className="overflow-auto scrollBar-hidden">
+                <div className="inline-flex gap-[22px] mt-[15px]">
+                  {selectedVarientElements.map((variant: any) => {
+                    return <>
                       <Selections
-                        title="Default Selections"
-                        products={DefaultProducts}
+                        products={variant}
                       />
-                    </div> */}
-                  </>;
-                })}
+                      {/* <div className="py-4">
+                        <Selections
+                          title="Default Selections"
+                          products={DefaultProducts}
+                        />
+                      </div> */}
+                    </>;
+                  })}
+                </div>
               </div>
             </div>
           </div>
