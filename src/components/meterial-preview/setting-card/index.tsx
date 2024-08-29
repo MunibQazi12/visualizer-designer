867115ff0583e91e0019ed8641fbf78130c38f97
MunibQazi12 <munibqazi770@gmail.com>
2024-08-29 17:29:12 +0500

    settings card scroll issue fixed

--- src/components/meterial-preview/setting-card/index.tsx
+++ src/components/meterial-preview/setting-card/index.tsx
@@ -74,53 +74,67 @@ const SettingCard = (props: I_Props) => {
       <div className={`flex justify-end pr-[33px] transition`}>
         <div className="bg-customWhite-600 mr-4 w-[414px] h-[493px] overflow-auto py-[35px] px-[38px] rounded-[28px] scrollBar-hidden">
           <p className="text-primary-100 font-semibold text-lg leading-[27px] font-roboto mb-[7px]">
-            {!!availableElements.length &&
-              availableElements[0][" Opt Sel Name"]}
+            {!!selectedVarientElements.length &&
+              selectedVarientElements[0][" Opt Sel Name"]}
           </p>
           <div>
             <div>
-              <p className="text-primary-100 font-semibold text-sm leading-[21px] font-roboto mb-0">
-                Active Selection
-              </p>
 
-              <div className="overflow-auto scrollBar-hidden">
-                <div className="inline-flex gap-[22px] mt-[15px]">
-                  {selectedElements.map((variant: any) => {
-                    return (
-                      <Selections
-                        key={variant[" Opt Val Name"]}
-                        elementsToDisplay={variant}
-                        className={"selected-img"}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
+              {
+                !!selectedElements.length ?
+                  <>
+                    <p className="text-primary-100 font-semibold text-sm leading-[21px] font-roboto mb-0">
+                      Active Selection
+                    </p>
+
+                    <div className="overflow-auto scrollBar-hidden">
+                      <div className="inline-flex gap-[22px] mt-[15px]">
+                        {selectedElements.map((variant: any) => {
+                          return (
+                            <Selections
+                              key={variant[" Opt Val Name"]}
+                              elementsToDisplay={variant}
+                              className={"selected-img"}
+                            />
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </>
+                  : null
+              }
+
             </div>
 
             <div>
-              <p className="text-primary-100 font-semibold text-sm leading-[21px] font-roboto mb-0">
-                Default Selection
-              </p>
 
-              <div className="overflow-auto scrollBar-hidden">
-                <div className="inline-flex gap-[22px] mt-[15px]">
-                  {availableElements.map((variant: any) => {
-                    return (
-                      <Selections
-                        key={variant[" Opt Val Name"]}
-                        elementsToDisplay={variant}
-                        selectedSelectedElements={
-                          selectedSelectedElementsHandler
-                        }
-                        className={""}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
+              {
+                !!availableElements.length ?
+                  <><p className="text-primary-100 font-semibold text-sm leading-[21px] font-roboto mb-0">
+                    Default Selection
+                  </p>
+
+                    <div className="overflow-auto scrollBar-hidden">
+                      <div className="inline-flex gap-[22px] mt-[15px]">
+                        {availableElements.map((variant: any) => {
+                          return (
+                            <Selections
+                              key={variant[" Opt Val Name"]}
+                              elementsToDisplay={variant}
+                              selectedSelectedElements={
+                                selectedSelectedElementsHandler
+                              }
+                              className={""}
+                            />
+                          );
+                        })}
+                      </div>
+                    </div></>
+                  : null
+              }
+
             </div>
-            
+
           </div>
         </div>
         <Activites FeaturesElements={FeaturesElements} />
